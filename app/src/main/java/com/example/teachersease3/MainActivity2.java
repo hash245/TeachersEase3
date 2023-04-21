c5bef2186d7d07096477c9e7a611096d5e735fdd
hash245 <131343908+hash245@users.noreply.github.com>
2023-04-21 14:15:26 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/teachersease3/MainActivity2.java
@@ -0,0 +1,40 @@
+package com.example.teachersease3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admim and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity2.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else
+                    //incorrect
+                    Toast.makeText(MainActivity2.this, "LOGIN FAILED!!!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
